3430f1a32d02f76f6f5bfe8c6851377f4b93ec92
sai wanza <q020385791@gmail.com>
2020-10-31 16:36:47 +0800

    加入專案檔案。

---
+++ WanWebLib/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WanWebLib
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+              name: "Home",
+              url: "{controller}/{action}/{id}",
+              defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+               name: "Word",
+               url: "Word/{controller}/{action}/{id}",
+               defaults: new { controller = "Word", action = "Index", id = UrlParameter.Optional }
+           );
+          
+           
+        }
+    }
+}
---
+++ WanWebLib/Controllers/DOC/WordController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WanWebLib.Controllers.DOC
+{
+    public class WordController : Controller
+    {
+        //Word控制工具
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //修改並直接下載
+        [HttpPost]
+        public ActionResult DownLoadWordFile()
+        {
+            
+
+            return View();
+        }
+    }
+}
